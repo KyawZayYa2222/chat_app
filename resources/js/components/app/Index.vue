4344c840dc48b4dc12a62de94ec2c8a3655755de
KyawZayYa <kyawzayya100417@gmail.com>
2024-05-22 22:23:24 +0630

    websocket setup

--- resources/js/components/app/Index.vue
+++ resources/js/components/app/Index.vue
@@ -1,3 +1,26 @@
 <template>
     <h1 class="text-black">main app layout</h1>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    },
+    mounted() {
+        this.testListening()
+    },
+    methods: {
+        testListening() {
+            console.log('listening')
+            window.Echo.channel('testing')
+                       .listen('TestFlight', (e) => {
+                            console.log('success')
+                            console.log(e.message)
+                       })
+        }
+    }
+}
+</script>
